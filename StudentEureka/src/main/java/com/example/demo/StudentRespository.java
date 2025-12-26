08770721e815a5c5f85860c5a26dbcc5f0c4e10a
Ramasubramaniyan12 <113416742+Ramasubramaniyan12@users.noreply.github.com>
2025-12-26 17:04:20 +0530

    Eureka Server Application with H2 DB

---
+++ StudentEureka/src/main/java/com/example/demo/StudentRespository.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRespository extends JpaRepository< StudentEntity, Long>{
+
+}
+
+
+
+
